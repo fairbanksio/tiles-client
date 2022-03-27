f779961a4f219f8853b654c0ba6a6b113add3a25
bsord <brandon.sorgdrager@gmail.com>
2022-03-27 12:43:11 -0500

    Add auth context to unit test

--- src/index.test.js
+++ src/index.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import AuthProvider from './contexts/AuthContext';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<AuthProvider><App /></AuthProvider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
